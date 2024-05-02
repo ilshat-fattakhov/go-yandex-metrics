2bba71335b33d82b6bd99d346159dba07ec9a044
ilshat-fattakhov <ilshat@fattakhov.com>
2024-05-02 23:56:06 +0800

    Storage funcs rewritten

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -12,34 +12,27 @@ import (
 )
 
 var logger = slog.New(slog.NewTextHandler(os.Stdout, nil))
-var m = storage.NewMemStorage()
+var mem = storage.NewMemStorage()
 
 func main() {
-	router := chi.NewRouter()
-	router.Post("/update/{mtype}/{mname}/{mvalue}", updateHandler) // POST /update/counter/PollCount/1
-	router.Get("/value/{mtype}/{mname}", getHandler)               // GET /value/counter/PollCount
-	router.Get("/", indexHandler)
+	r := chi.NewRouter()
+	r.Post("/update/{mtype}/{mname}/{mvalue}", updateHandler) // POST /update/counter/PollCount/1
+	r.Get("/value/{mtype}/{mname}", getHandler)               // GET /value/counter/PollCount
+	r.Get("/", indexHandler)
 
-	//logger.Info("Getting values from chi: " + mType + " Name: " + mName + " Value: " + mValue)
-
-	log.Fatal(http.ListenAndServe(":8080", router))
+	log.Fatal(http.ListenAndServe(":8080", r))
 	//log.Fatal(http.ListenAndServe(":8080", updateRouter()))
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-
 	if r.Method != http.MethodGet {
-		// Принимаем запросы только по протоколу HTTP методом GET
-		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.WriteHeader(http.StatusMethodNotAllowed) // Принимаем запросы только по протоколу HTTP методом GET
 		return
 	}
-
 	logger.Info("Listing all counters with values...")
-
 	// По запросу GET http://<АДРЕС_СЕРВЕРА>/ сервер должен отдавать HTML-страницу
 	// со списком имён и значений всех известных ему на текущий момент метрик.
 	w.Write([]byte("Gauge:\n"))
-
 	for mName, mValue := range storage.GaugeMetrics {
 		w.Write([]byte(mName + ":" + fmt.Sprint(mValue) + "\n"))
 	}
@@ -48,6 +41,7 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(mName + ":" + fmt.Sprint(mValue) + "\n"))
 	}
 }
+
 func getHandler(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method != http.MethodGet {
@@ -55,59 +49,26 @@ func getHandler(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
-
-	// Принимать запрос в формате http://<АДРЕС_СЕРВЕРА>/value/<ТИП_МЕТРИКИ>/<ИМЯ_МЕТРИКИ>
-
 	mType := chi.URLParam(r, "mtype")
 	mName := chi.URLParam(r, "mname")
+	logger.Info("Got type:" + chi.URLParam(r, "mtype") + chi.URLParam(r, "mname"))
+	//mValue := mem.Get(w, r)
+	mValue := mem.Get(mType, mName)
 
-	logger.Info("Get request. Type: " + mType + " Name: " + mName)
-	logger.Info("We have a visitor: " + r.RequestURI)
-
-	if mType == "" && mName == "" {
-		logger.Info("No metric type and no metric name")
-		w.WriteHeader(http.StatusNotFound)
+	if mValue == "" {
+		logger.Info("Oops!")
+		w.WriteHeader(http.StatusBadRequest)
+		//logger.Info("При попытке запроса метрики без значения сервер должен возвращать http.StatusNotFound ?")
+		//w.WriteHeader(http.StatusNotFound)
 		return
-	}
-	logger.Info(" with mType: " + mType)
-
-	if mType == "gauge" || mType == "counter" {
-		mValue := getCounterValue(mType, mName)
-		if mValue == "" {
-			logger.Info("При попытке запроса метрики без значения сервер должен возвращать http.StatusNotFound ?")
-			w.WriteHeader(http.StatusNotFound)
-		} else {
-			logger.Info("Sending metrics to browser. Type: " + mType + " Name: " + mName + " Value: " + mValue)
-
-			w.Header().Set("content-type", "text/plain")
-			w.Header().Set("charset", "utf-8")
-			//w.Header().Set("content-length", "11")
-			w.WriteHeader(http.StatusOK)
-			w.Write([]byte(mValue + "\n"))
-
-		}
-		return
-
 	} else {
-		// При попытке запроса неизвестной метрики сервер должен возвращать http.StatusNotFound.
-		logger.Info("При попытке запроса неизвестной метрики сервер должен возвращать http.StatusNotFound.")
-		w.WriteHeader(http.StatusNotFound)
+		w.Header().Set("content-type", "text/plain")
+		w.Header().Set("charset", "utf-8")
+		//w.Header().Set("content-length", "11")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(mValue))
 		return
 	}
-
-}
-
-func getCounterValue(mType, mName string) string {
-	if mType == "gauge" {
-		if mValue, ok := storage.GaugeMetrics[mName]; ok {
-			return fmt.Sprint(mValue)
-		}
-	} else if mType == "counter" {
-		if mValue, ok := storage.GaugeMetrics[mName]; ok {
-			return fmt.Sprint(mValue)
-		}
-	}
-	return ""
 }
 
 func updateHandler(w http.ResponseWriter, r *http.Request) {
@@ -117,50 +78,14 @@ func updateHandler(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
-
 	// Принимать данные в формате http://<АДРЕС_СЕРВЕРА>/update/<ТИП_МЕТРИКИ>/<ИМЯ_МЕТРИКИ>/<ЗНАЧЕНИЕ_МЕТРИКИ>
 	mType := chi.URLParam(r, "mtype")
 	mName := chi.URLParam(r, "mname")
 	mValue := chi.URLParam(r, "mvalue")
-
-	logger.Info("We have a visitor: " + r.RequestURI)
-	logger.Info("Saving metrics. Type: " + mType + " Name: " + mName + " Value: " + mValue)
-
-	//if mType == "" && mName == "" && mValue == "" {
-	//	w.WriteHeader(http.StatusOK) // PROBABLY WRONG!!!
-	//	return
-	//}
-	//logger.Info(" with mType: " + mType)
-
 	if mType == "gauge" || mType == "counter" {
-		if mName != "" {
-			if mValue != "" {
-				err := m.Save(mType, mName, mValue)
-				if err != nil {
-					// При попытке передать запрос с некорректным значением возвращать http.StatusBadRequest
-					w.WriteHeader(http.StatusBadRequest)
-					return
-				} else {
-					logger.Info("Saving metrics. Type: " + mType + " Name: " + mName + " Value: " + mValue)
-					w.Header().Set("Content-Length", "0")
-					w.Header().Set("Content-Type", "text/plain")
-					w.Header().Set("Charset", "utf-8")
-					w.WriteHeader(http.StatusOK)
-					return
-				}
-			} else {
-				// При попытке передать запрос с пустым значением возвращать http.StatusBadRequest
-				w.WriteHeader(http.StatusBadRequest)
-				return
-			}
-		} else {
-			logger.Info("При попытке передать запрос без имени метрики возвращать http.StatusNotFound")
-			w.WriteHeader(http.StatusNotFound)
-			return
-		}
+		mem.Save(mType, mName, mValue)
 	} else {
-		logger.Info("При попытке передать запрос с некорректным типом метрики возвращать http.StatusBadRequest")
 		w.WriteHeader(http.StatusBadRequest)
-		return
 	}
+
 }
--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,6 +1,10 @@
 package storage
 
 import (
+	"fmt"
+	//"go-yandex-metrics/internal/storage"
+	"log/slog"
+	"os"
 	"strconv"
 )
 
@@ -11,39 +15,109 @@ type MemStorage struct {
 	counter map[string]int64
 }
 
+var logger = slog.New(slog.NewTextHandler(os.Stdout, nil))
+
 func NewMemStorage() *MemStorage {
 	return &MemStorage{
 		gauge:   make(map[string]float64),
 		counter: make(map[string]int64),
 	}
 }
+func (m *MemStorage) saveCounter(mName, mValue string) {
 
-func (m *MemStorage) Save(t, n, v string) error {
+	// в случае если мы по какой-то причине получили число с плавающей точкой
+	vFloat64, _ := strconv.ParseFloat(mValue, 64)
+	vInt64 := int64(vFloat64)
+	// новое значение должно добавляться к предыдущему, если какое-то значение уже было известно серверу
+	m.counter[mName] += vInt64
+	//return nil
+}
 
-	if t == "counter" {
+func (m *MemStorage) saveGauge(mName, mValue string) {
 
-		// в случае если мы по какой-то причине получили число с плавающей точкой
-		vFloat64, err := strconv.ParseFloat(v, 64)
-		if err != nil {
-			return err
-		}
-		vInt64 := int64(vFloat64)
-		// новое значение должно добавляться к предыдущему, если какое-то значение уже было известно серверу
-		m.counter[n] += vInt64
-		return nil
+	vFloat64, _ := strconv.ParseFloat(mValue, 64)
+	// новое значение должно замещать предыдущее
+	m.gauge[mName] = vFloat64
+	//return nil
+}
 
-	} else if t == "gauge" {
+// func (m *MemStorage) Save(t, n, v string) error {
+func (m *MemStorage) Save(mType, mName, mValue string) {
 
-		vFloat64, err := strconv.ParseFloat(v, 64)
-		if err != nil {
-			return err
-		}
-		// новое значение должно замещать предыдущее
-		m.gauge[n] = vFloat64
-		return nil
+	logger.Info("Saving:" + mType + mName + mValue)
+
+	if mType == "counter" {
+		m.saveCounter(mName, mValue)
+	} else if mType == "gauge" {
+		m.saveGauge(mName, mValue)
+	}
+}
+
+func (m *MemStorage) Get(mType, mName string) string {
+
+	logger.Info("Got type:" + mType + " and name " + mName)
+
+	if mType == "counter" {
+		logger.Info("Getting counter value")
+		return (GetCounter(mName))
+	} else if mType == "gauge" {
+		logger.Info("Getting gauge value")
+		return (GetGauge(mName))
+	} else {
+		return ""
+	}
+
+}
+
+func GetCounter(mName string) string {
+
+	// Принимать запрос в формате http://<АДРЕС_СЕРВЕРА>/value/<ТИП_МЕТРИКИ>/<ИМЯ_МЕТРИКИ>
+	logger.Info("Get request. Type: Counter, Name: " + mName)
+	mValue := getCounterValue("counter", mName)
+	if mValue == "" {
+		return ""
+	} else {
+		logger.Info("Sending metrics to browser. Type: Counter, Name: " + mName + " Value: " + mValue)
+		return mValue
+	}
+
+}
+
+func GetGauge(mName string) string {
+
+	// Принимать запрос в формате http://<АДРЕС_СЕРВЕРА>/value/<ТИП_МЕТРИКИ>/<ИМЯ_МЕТРИКИ>
+	logger.Info("Get request. Type: Gauge" + " Name: " + mName)
+	mValue := getCounterValue("gauge", mName)
+	if mValue == "" {
+		return ""
+	} else {
+		logger.Info("Sending metrics to browser. Type Gauge, Name: " + mName + " Value: " + mValue)
+		return mValue
 
 	}
-	return nil
+
+}
+func getCounterValue(mType, mName string) string {
+	logger.Info("getCounterValue func: Type Name: " + mType + mName)
+
+	if mType == "gauge" {
+		if mValue, ok := GaugeMetrics[mName]; ok {
+			return fmt.Sprint(mValue)
+		}
+	} else if mType == "counter" {
+		logger.Info("We are in counter: " + mType + mName)
+
+		if mValue, ok := CounterMetrics[mName]; ok {
+
+			logger.Info("OK" + string(mValue))
+			return fmt.Sprint(mValue)
+		} else {
+
+			logger.Info("NOT OK")
+
+		}
+	}
+	return ""
 }
 
 var GaugeMetrics = map[string]float64{
