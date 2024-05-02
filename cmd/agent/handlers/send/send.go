d65d736244fd9207d57177b0d37e70c187de5cdd
ilshat-fattakhov <ilshat@fattakhov.com>
2024-05-02 12:01:27 +0800

    Send function fix

--- cmd/agent/handlers/send/send.go
+++ cmd/agent/handlers/send/send.go
@@ -23,6 +23,8 @@ func SendMetrics() {
 
 		url := "http://localhost:8080/update/gauge/" + n + "/" + fmt.Sprintf("%v", v)
 
+		logger.Info("Sending gauge metrics to URL: " + url + "...")
+
 		req, err := http.NewRequest("POST", url, nil)
 		if err != nil {
 			logger.Info(fmt.Sprintf("error %s", err))
