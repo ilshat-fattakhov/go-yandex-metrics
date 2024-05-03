a02d84991cca9b7b8f43c4250a24223e740d8508
ilshat-fattakhov <ilshat@fattakhov.com>
2024-05-03 17:53:02 +0800

    Didn't work! Rolling back...

--- cmd/agent/handlers/send/send.go
+++ cmd/agent/handlers/send/send.go
@@ -44,13 +44,13 @@ func SendMetrics() {
 
 		logger.Info("After http.NewRequest...")
 
-		_, err = c.Do(req)
+		resp, err := c.Do(req)
 		if err != nil {
 			logger.Info(fmt.Sprintf("error %s", err))
 			continue
 		}
 
-		//defer resp.Body.Close()
+		defer resp.Body.Close()
 
 	}
 
