5f63bff8e7043cae226049bd8805c9b4d7c1271d
ilshat-fattakhov <ilshat@fattakhov.com>
2024-05-30 23:22:19 +0800

    Trying again

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"log"
-
 	logger "go-yandex-metrics/cmd/server/middleware"
 	"go-yandex-metrics/internal/config"
 	"go-yandex-metrics/internal/server/api"
 	"go-yandex-metrics/internal/storage"
+
+	"go.uber.org/zap"
 )
 
 func main() {
@@ -15,22 +15,22 @@ func main() {
 	lg.Info("creating configuration")
 	cfg, storageCfg, err := config.NewServerConfig()
 	if err != nil {
-		lg.Info("got error creating configuration")
-		log.Fatalf("failed to create config: %v", err)
+		lg.Info("got error creating configuration", zap.Error(err))
+		// log.Fatalf("failed to create config: %v", err)
 	}
 
 	store := storage.NewFileStorage()
 	lg.Info("creating server")
 	server, err := api.NewServer(cfg, storageCfg, store)
 	if err != nil {
-		lg.Info("got error creating server")
-		log.Fatalf("failed to create server: %v", err)
+		lg.Info("got error creating server", zap.Error(err))
+		// log.Fatalf("failed to create server: %v", err)
 	}
 
 	lg.Info("starting server")
 	err = server.Start()
 	if err != nil {
-		lg.Info("got error starting server")
-		log.Fatalf("failed to start server %v", err)
+		lg.Info("got error starting server", zap.Error(err))
+		// log.Fatalf("failed to start server %v", err)
 	}
 }
