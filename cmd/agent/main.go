40e0f86f558effb8b4adc1cdac2c1066ec79693e
ilshat-fattakhov <ilshat@fattakhov.com>
2024-06-07 10:49:00 +0800

    Checking if linter error: "Text exitAfterDefer: log.Fatalf will exit, and `defer func(){...}(...)` will not run" is still there

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -20,17 +20,17 @@ func main() {
 	cfg, err := config.NewAgentConfig()
 	lg.Info("agent configuration settings" + fmt.Sprint(cfg))
 	if err != nil {
-		log.Panicf("failed to create config: %v", err)
+		log.Fatalf("failed to create config: %v", err)
 	}
 
 	store, err := api.NewAgentMemStorage(cfg)
 	if err != nil {
-		log.Panicf("got error creating storage %v", err)
+		log.Fatalf("got error creating storage %v", err)
 	}
 	agent := api.NewAgent(cfg, store)
 
 	err = agent.Start()
 	if err != nil {
-		log.Panicf("failed to start agent %v", err)
+		log.Fatalf("failed to start agent %v", err)
 	}
 }
