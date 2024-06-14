8d99a55fb70660eac2c3af29e571bdfb260c090c
ilshat-fattakhov <ilshat@fattakhov.com>
2024-06-14 16:10:52 +0800

    Increment 13 first commit

--- internal/agent/api/agent.go
+++ internal/agent/api/agent.go
@@ -3,6 +3,7 @@ package api
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 	"sync"
 	"time"
 
@@ -52,7 +53,7 @@ func NewAgent(cfg config.AgentCfg, store *MemStorage) (*Agent, error) {
 func (a *Agent) Start() error {
 	tickerSave := time.NewTicker(time.Duration(a.cfg.PollInterval) * time.Second)
 	tickerSend := time.NewTicker(time.Duration(a.cfg.ReportInterval) * time.Second)
-	batchSend := time.NewTicker(time.Duration(4) * time.Second)
+	batchSend := time.NewTicker(time.Duration(a.cfg.ReportInterval) * time.Second)
 
 	for {
 		select {
@@ -61,12 +62,33 @@ func (a *Agent) Start() error {
 		case <-tickerSend.C:
 			err := a.sendMetrics()
 			if err != nil {
-				a.logger.Error("failed to send metrics: %w", zap.Error(err))
+				a.logger.Error("failed to send metrics, trying again: %w", zap.Error(err))
+				if a.cfg.ReportInterval > 1 {
+					for i := 1; i <= 5; i += 2 {
+						if i > int(a.cfg.ReportInterval) {
+							break
+						}
+						time.Sleep(time.Duration(i) * time.Second)
+						err := a.sendMetrics()
+						if err != nil {
+							a.logger.Error("failed to send metrics after "+strconv.Itoa(i)+" second(s)", zap.Error(err))
+						}
+					}
+				}
 			}
 		case <-batchSend.C:
 			err := a.sendMetricsBatch()
+			i := 1
+			for err != nil {
+				time.Sleep(time.Duration(i) * time.Second)
+				err = a.sendMetricsBatch()
+				i += 2
+				if i >= 5 || i > int(a.cfg.ReportInterval) {
+					break
+				}
+			}
 			if err != nil {
-				a.logger.Error("failed to send metrics in batch: %w", zap.Error(err))
+				a.logger.Error("failed to send a batch of metrics after "+strconv.Itoa(i)+" second(s)", zap.Error(err))
 			}
 		}
 	}
